9504f0dfe6da63a09122b84b0ab7aec441ee6c17
Fouad Matin <fm@fouad.co>
2015-07-22 23:35:36 -0700

    fix

--- app/scripts/contentscript.js
+++ app/scripts/contentscript.js
@@ -1,8 +1,18 @@
 'use strict';
 
 function extract() {
-  var pagelet = document.getElementById('stream_pagelet')
+  var pagelet = document.getElementById('stream_pagelet');
+
+  if (!pagelet) {
+    return;
+  }
+
   var divs = pagelet.querySelectorAll(':scope > div')
+
+  if (!divs || divs.length < 1) {
+    return;
+  }
+
   var stream = divs[divs.length - 1]
   var rightCol = document.getElementById('rightCol');
 
